fd7f4745b4b0a924658b679a8151d1ae7fcbe424
gazizkv <valitovgaziz@gmail.com>
2021-07-12 19:39:23 +0500

    add parse and get asessor class

---
+++ src/main/java/Asessor.java
@@ -0,0 +1,15 @@
+public class Asessor {
+    String login;
+    Integer uid;
+    Short docid;
+    Short jud;
+    Short cjud;
+
+    public Asessor(String login, Integer uid, Short docid, Short jud, Short cjud) {
+        this.login = login;
+        this.uid = uid;
+        this.docid = docid;
+        this.jud = jud;
+        this.cjud = cjud;
+    }
+}
--- src/main/java/secondTaskSolution.java
+++ src/main/java/secondTaskSolution.java
@@ -1,5 +1,35 @@
-public class secondTaskSolution {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
 
+public class secondTaskSolution {
+    public static ArrayList<Asessor> listOfAsessors = new ArrayList<Asessor>();
+
+    public static Asessor getNewAsessor(String[] nextLine) {
+        Asessor newAsessor = new Asessor(nextLine[0],
+                Integer.parseInt(nextLine[1]),
+                Short.parseShort(nextLine[2]),
+                Short.parseShort(nextLine[3]),
+                Short.parseShort(nextLine[4]));
+        return newAsessor;
+    }
+
+    public static void main(String[] args) {
+        File file =  new File("C:\\Users\\valit\\secondTestTaskOnProgetMenegerFromYandex\\src\\main\\resources\\файл 2.csv");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String[] nextLine;
+            while (bufferedReader.ready()) {
+                nextLine = bufferedReader.readLine().split(" ");
+                listOfAsessors.add(getNewAsessor(nextLine));
+            }
+            for (int i = 0; i < 10; i++) {
+                System.out.println(listOfAsessors.get(i).login);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
